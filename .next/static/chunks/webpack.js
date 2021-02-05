ae382f117b7aa556cb9434f0a521671dcf99de53
victorlaragurmendi <lince00@outlook.com>
2021-02-04 20:15:23 -0500

    cambio en la configuracion de la bd

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "58fb392647218bd2bcc1";
+/******/ 	var hotCurrentHash = "cea634be8400517a1f24";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- conexion.js
+++ conexion.js
@@ -41,4 +41,58 @@ connection.connect(err=>{
 // });
  
 
-module.exports = connection;
+// module.exports = connection;
+
+// const connection=require('../conexion')
+const cors = require('cors');
+
+
+const  express = require('express')
+
+const bodyParser=require('body-parser')
+  const PORT =process.env.PORT || 3001
+
+  const app=express();
+  app.use(cors());
+  app.use(bodyParser.json());
+
+  app.listen(PORT,()=>console.log(`Server runing o port ${PORT}`))
+
+  //rutas
+  app.get('/',(req,res)=>{
+    res.send('WELCOME  TO MY API')
+  })
+
+  app.get('/productos',(req,res)=>{
+      const sql ='SELECT * from  articulos'
+      connection.query(sql,(error,results)=>{
+          if(error) throw error;
+          if(results.length>0){
+               res.json(results)
+          }
+          else{
+              res.send('Not results')
+          }
+      })
+      
+  })
+
+  app.post('/carts',(req,res)=>{
+    const sql ='INSERT INTO pedidos SET ?';
+    const pedidoObj={
+      total:req.body.total,
+      cuerpo:req.body.cuerpo
+      
+    };
+    connection.query(sql,pedidoObj,error =>{
+      if(error) throw error;
+      res.send('Pedido registrado con exito')
+    });
+    
+  });
+
+  app.post('/carrito/:id',(req,res)=>{
+    res.send('enviando productos del carito del cliente con Id:')
+  })
+
+  
